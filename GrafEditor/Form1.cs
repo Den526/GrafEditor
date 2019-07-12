bf8e3dd3ad22982dd89a135d4a71f12f19351285
Den526 <protos421@mail.ru>
2019-07-13 00:07:58 +0700

    Добавлена возможность перемещать концы и изломы линии Функции прорисовки вынесены в отдельный файл в partial class

--- GrafEditor/Form1.cs
+++ GrafEditor/Form1.cs
@@ -19,18 +19,19 @@ namespace GrafEditor
         Point PMoveBegin; // точки для рисования простой линии
         Point PMoveEnd;
         List<FractureLine> FL = new List<FractureLine>();
-        //List<FractureLine> FL_tmp = new List<FractureLine>();
         int IndActiveFL = -1;  //индекс активной линии, с которой в данный момент работает пользователь
         int IndActivePoint = -1;  //индекс активной точки, с которой в данный момент работает пользователь
         float ThicknesLine = 1;  //толщина активной линии
         Color ColorLine = Color.Black; //цвет активной линии
-        bool flMoveLine = false;
-        bool flAddLine = false;
-        bool flAddPoint = false;
+        
+        bool flMoveLine = false;  //флаг разрешения перемещения линии
+        bool flMovePoint = false; //флаг разрешения перемещения точки
+        bool flAddLine = false;  //флаг добавления линии
+        bool flAddPoint = false; //флаг добавления точки
 
         private void pbMainGrafWin_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void pbMainGrafWin_MouseClick(object sender, MouseEventArgs e)
@@ -45,6 +46,12 @@ namespace GrafEditor
                         FL[IndActiveFL].Move(e.X - PMoveBegin.X, e.Y - PMoveBegin.Y); //переместить всю ломаную на разницу перемещения
                         flMoveLine = false;  //отпустили мышь - хватит двигать
                         pbMainGrafWin.Image = PaintBitmap(pbMainGrafWin.Width, pbMainGrafWin.Height, FL);  //рисуем обновленную картину
+                    }
+                    if (flMovePoint)
+                    {
+                        FL[IndActiveFL].points[IndActivePoint] = new Point(e.X, e.Y);
+                        flMovePoint = false;
+                        pbMainGrafWin.Image = PaintBitmap(pbMainGrafWin.Width, pbMainGrafWin.Height, FL);
                     }
                     if (flAddPoint)
                     {
@@ -70,8 +77,26 @@ namespace GrafEditor
                 if (IndActiveFL >= 0)
                 {
                     PMoveBegin = new Point(e.X, e.Y);
-                    flMoveLine = true;
+                    for (int ti = 0; ti < FL.Count; ti++)
+                    {
+                        int dx = 0; int dy = 0;
+                        for (int pi = 0; pi < FL[ti].points.Count; pi++)
+                        {
+                            dx = e.X - FL[ti].points[pi].X;
+                            dy = e.Y - FL[ti].points[pi].Y;
+                            //будем считать что попали в точку, даже если есть отклонения в 1 пиксель в любую сторону
+                            if (Math.Abs(dx) <= 1 && Math.Abs(dy) <= 1)
+                            {
+                                IndActiveFL = ti;
+                                IndActivePoint = pi;
+                                flMovePoint = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!flMovePoint) flMoveLine = true;
                     //pbMainGrafWin.Image = PaintBitmap(pbMainGrafWin.Width, pbMainGrafWin.Height, FL);
+                    
                 }
             }
         }
@@ -80,15 +105,40 @@ namespace GrafEditor
             FractureLine FL_tmp = new FractureLine();   //временная линия
             if (IndActiveFL >= 0)
             {
+                FL_tmp.Pero = new Pen(Color.Silver, 1);
                 if (flMoveLine)
                 {
-                    PMoveEnd = new Point(e.X, e.Y);
-                    FL_tmp.Pero = new Pen(Color.Silver, 1);
+                    //PMoveEnd = new Point(e.X, e.Y);
+
                     for (int ti = 0; ti < FL[IndActiveFL].points.Count; ti++)
                     {
                         FL_tmp.AddPoint(FL[IndActiveFL].points[ti]);
                     } 
                     FL_tmp.Move(e.X - PMoveBegin.X, e.Y - PMoveBegin.Y);
+                    pbMainGrafWin.Image = PaintBitmap(pbMainGrafWin.Width, pbMainGrafWin.Height, FL);
+                    pbMainGrafWin.Image = PaintBitmap((Bitmap)pbMainGrafWin.Image, FL_tmp);
+                }
+                if (flMovePoint)
+                {
+                    if (IndActivePoint == 0)
+                    {
+                        //обработка перемещения первой точки
+                        FL_tmp.AddPoint(new Point(e.X, e.Y));
+                        FL_tmp.AddPoint(FL[IndActiveFL].points[IndActivePoint + 1]);
+                    }
+                    else if (IndActivePoint > 0 && IndActivePoint < FL[IndActiveFL].points.Count - 1)
+                    {
+                        //обработка перемещения излома
+                        FL_tmp.AddPoint(FL[IndActiveFL].points[IndActivePoint - 1]);
+                        FL_tmp.AddPoint(new Point(e.X, e.Y));
+                        FL_tmp.AddPoint(FL[IndActiveFL].points[IndActivePoint + 1]);
+                    }
+                    else if (IndActivePoint == FL[IndActiveFL].points.Count - 1)
+                    {
+                        //обработка перемещения первой точки
+                        FL_tmp.AddPoint(FL[IndActiveFL].points[IndActivePoint - 1]);
+                        FL_tmp.AddPoint(new Point(e.X, e.Y));
+                    }
                     pbMainGrafWin.Image = PaintBitmap(pbMainGrafWin.Width, pbMainGrafWin.Height, FL);
                     pbMainGrafWin.Image = PaintBitmap((Bitmap)pbMainGrafWin.Image, FL_tmp);
                 }
@@ -124,75 +174,14 @@ namespace GrafEditor
             listBox1.SelectedIndex = listBox1.Items.Count - 1;
         }
 
-        //*********************************************** Прорисовка полная *****************************************//
-        private Bitmap PaintBitmap(int width, int height, List<FractureLine> flt)
-        {
-            //русуем массив линий
-            Bitmap btmBack = new Bitmap(width, height);
-            Graphics grBack = Graphics.FromImage(btmBack);
-
-            Point p1 = new Point();
-            Point p2 = new Point();
-            Pen P;
-            Pen Ptch;
-            
-            for (int x = 0; x < flt.Count; x++)
-            {
-                P = flt[x].Pero;
-                Ptch = new Pen(Color.LimeGreen, (float)(flt[x].Pero.Width + 1));
-                if (flt[x].points.Count > 0) p1 = flt[x].points[0];  //начало рисования
-                for(int t = 0; t < flt[x].points.Count; t++)
-                {
-                    p2 = flt[x].points[t];
-                    Rectangle r1 = new Rectangle(new Point(p2.X - (int)(Ptch.Width / 2), p2.Y - (int)(Ptch.Width / 2)), new Size((int)Ptch.Width, (int)Ptch.Width));
-                    
-                    grBack.DrawLine(P, p1, p2);
-                    grBack.DrawEllipse(Ptch, r1);
-                    p1 = p2;
-                }
-            }
-            return btmBack;
-        }
-        //*********************************************** Дорисовка *****************************************//
-        private Bitmap PaintBitmap(Bitmap btmBack, FractureLine flt)
-        {
-            //добавить на существующий битмап одну линию
-            //нужна для временного отображения ожидаемого результата (рисования отрезка. Или 2-х отрезков, если переносится промежуточная точка
-            Graphics grBack = Graphics.FromImage(btmBack);
-
-            Point p1 = new Point();
-            Point p2 = new Point();
-            Pen P;
-            Pen Ptch;
-
-            //int mx = 1; //Масштаб по x и y
-            //int my = 1;
-
-            //for (int x = 0; x < flt.Count; x++)
-            {
-                P = flt.Pero;
-                Ptch = new Pen(Color.LimeGreen, (float)(flt.Pero.Width + 1));
-                if (flt.points.Count > 0) p1 = flt.points[0];  //начало рисования
-                for (int t = 0; t < flt.points.Count; t++)
-                {
-                    p2 = flt.points[t];
-                    Rectangle r1 = new Rectangle(new Point(p2.X - (int)(Ptch.Width / 2), p2.Y - (int)(Ptch.Width / 2)), new Size((int)Ptch.Width, (int)Ptch.Width));
-
-                    grBack.DrawLine(P, p1, p2);
-                    grBack.DrawEllipse(Ptch, r1);
-                    p1 = p2;
-                }
-            }
-            return btmBack;
-        }
-        //******************************************************************************************************//
 
         private void SelectColor_Click(object sender, EventArgs e)
         {
+            //тут пока плохо работает
             //выбор цвета линии кликами по лейблам
-            p1 = new Pen(label11.BackColor, Convert.ToInt16(cbThicknes.Text));
-            toolStripStatusLabel1.BackColor = label11.BackColor;
-            if (IndActiveFL >= 0) FL[IndActiveFL].Pero = p1;
+            //p1 = new Pen(label11.BackColor, Convert.ToInt16(cbThicknes.Text));
+            //toolStripStatusLabel1.BackColor = label11.BackColor;
+            //if (IndActiveFL >= 0) FL[IndActiveFL].Pero = p1;
         }
 
         private void cmdSelectColor_Click(object sender, EventArgs e)
---
+++ GrafEditor/PaintBitmap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GrafEditor
+{
+    public partial class Form1
+    {
+        //*********************************************** Прорисовка полная *****************************************//
+        private Bitmap PaintBitmap(int width, int height, List<FractureLine> flt)
+        {
+            //русуем массив линий
+            Bitmap btmBack = new Bitmap(width, height);
+            Graphics grBack = Graphics.FromImage(btmBack);
+
+            Point p1 = new Point();
+            Point p2 = new Point();
+            Pen P;
+            Pen Ptch;
+
+            for (int x = 0; x < flt.Count; x++)
+            {
+                P = flt[x].Pero;
+                Ptch = new Pen(Color.LimeGreen, (float)(flt[x].Pero.Width + 1));
+                if (flt[x].points.Count > 0) p1 = flt[x].points[0];  //начало рисования
+                for (int t = 0; t < flt[x].points.Count; t++)
+                {
+                    p2 = flt[x].points[t];
+                    Rectangle r1 = new Rectangle(new Point(p2.X - (int)(Ptch.Width / 2), p2.Y - (int)(Ptch.Width / 2)), new Size((int)Ptch.Width, (int)Ptch.Width));
+
+                    grBack.DrawLine(P, p1, p2);
+                    grBack.DrawEllipse(Ptch, r1);
+                    p1 = p2;
+                }
+            }
+            return btmBack;
+        }
+        //*********************************************** Дорисовка *****************************************//
+        private Bitmap PaintBitmap(Bitmap btmBack, FractureLine flt)
+        {
+            //добавить на существующий битмап одну линию
+            //нужна для временного отображения ожидаемого результата (рисования отрезка. Или 2-х отрезков, если переносится промежуточная точка
+            Graphics grBack = Graphics.FromImage(btmBack);
+
+            Point p1 = new Point();
+            Point p2 = new Point();
+            Pen P;
+            Pen Ptch;
+
+            //int mx = 1; //Масштаб по x и y
+            //int my = 1;
+
+            //for (int x = 0; x < flt.Count; x++)
+            {
+                P = flt.Pero;
+                Ptch = new Pen(Color.LimeGreen, (float)(flt.Pero.Width + 1));
+                if (flt.points.Count > 0) p1 = flt.points[0];  //начало рисования
+                for (int t = 0; t < flt.points.Count; t++)
+                {
+                    p2 = flt.points[t];
+                    Rectangle r1 = new Rectangle(new Point(p2.X - (int)(Ptch.Width / 2), p2.Y - (int)(Ptch.Width / 2)), new Size((int)Ptch.Width, (int)Ptch.Width));
+
+                    grBack.DrawLine(P, p1, p2);
+                    grBack.DrawEllipse(Ptch, r1);
+                    p1 = p2;
+                }
+            }
+            return btmBack;
+        }
+        //******************************************************************************************************//
+
+    }
+}
